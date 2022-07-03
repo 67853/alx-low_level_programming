939847eae5effe1ca3fbfc3ec6471783fb24b46f
Andima Geoffrey <geoffreyandima@gmail.com>
2022-07-02 17:47:33 -0700

    Andima

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ long int l;
 long long int ll;
 float d;
 char c;
+printf("Size of a char: %lu byte(s)\n", sizeof(c));
 printf("Size of an int: %lu byte(s)\n", sizeof(i));
 printf("Size of a long int: %lu byte(s)\n", sizeof(l));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(ll));
