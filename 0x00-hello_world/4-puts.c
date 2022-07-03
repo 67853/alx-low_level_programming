ed1c5bc97023aebe243fd3a7e761ed35c9877039
Andima Geoffrey <geoffreyandima@gmail.com>
2022-07-03 02:22:30 -0700

    Andima

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
 /**
- * main - Entry point
- * Description: prints "\"Programming
- * Returns: Always 0 (Success)
+ * main - Prints a statement
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-
 return (0);
 }
