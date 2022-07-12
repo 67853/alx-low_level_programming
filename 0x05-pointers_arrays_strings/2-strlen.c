8d20c3a310a3fa078e75002f156ab67950ad0c1e
Andima Geoffrey <geoffreyandima@gmail.com>
2022-07-12 05:13:30 -0700

    strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strlen - Returns the length of a string.
+ * @s: The string to get the length of.
+ * eturn: The length of @str.
+ */
+
+int _strlen(char *s)
+{
+int len = 0;
+
+
+while (*s++)
+{
+len++;
+}
+return (len);
+}
